d3d0175d89aefb53176fb71dcc21331552af0f35
NoahY <noahy57@gmail.com>
2011-08-12 21:50:11 +0530

    css

--- qa-print-layer.php
+++ qa-print-layer.php
@@ -19,8 +19,14 @@
 		{
 			if($this->pr) {
 				$this->output('<style>
-				
-				
+					.qa-vote-count {margin-bottom: 12px;}
+					.qa-q-view-tags {margin-bottom:12px;}
+					.qa-q-view-tag-list {list-style:none; margin:0; padding:0;}
+					.qa-q-view-tag-item {display:inline;}	
+					.qa-a-item-content {font-size:14px; margin-bottom:12px;}
+					.qa-q-view-meta {margin-bottom:12px;}
+					.qa-a-item-meta {margin-bottom:12px;}
+					.qa-c-list-item {border-bottom:1px dotted #666; padding:6px; padding-right:0;}								
 				</style>');
 			}
 			else qa_html_theme_base::head_css();
@@ -163,7 +169,6 @@
 				elseif ($a_item['selected'])
 					$this->output('<DIV CLASS="qa-a-item-selected">');
 
-				$this->a_selection($a_item);
 				$this->a_item_content($a_item);
 				$this->post_meta($a_item, 'qa-a-item');
 				$this->a_item_clear();
